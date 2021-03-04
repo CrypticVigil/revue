98c1c433ff630e11d3bcb9a1a82dea89147e94f7
Chad Barrows <chad.m.barrows@gmail.com>
2021-03-03 19:15:55 -0500

    addMovie form is working

--- addMovie.php
+++ addMovie.php
@@ -1,44 +1,103 @@
 <?php
 
-	include 'db_connect.php';
+include 'db_connect.php';
 
-	$email = $crust = '';
-	$toppings = [];
-	$error = false;
+// $email = $crust = '';
+// $toppings = [];
+// $error = false;
 
-	if (isset( $_POST['submit'] )) {
-		print_r($_POST);
+// if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+// 	echo 'Email address is valid.';
+// 	$email = mysqli_real_escape_string($conn, $_POST['email']);
+// } else {
+// 	echo 'Email address is not valid.';
+// 	$error = true;
+// }
+// $crust = $_POST['crust'];
 
-		if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			echo 'Email address is valid.';
-			$email = mysqli_real_escape_string($conn, $_POST['email']);
+// for ($i = 1; $i < 5; $i++) {
+// 	array_push($toppings, $_POST["topping$i"]);
+// }
+
+// $toppings = array_filter($toppings);
+
+// if (!$error) {
+// 	$topString = implode(', ', $toppings);
+
+// 	// create SQL query
+// 	$sql = "INSERT INTO orders (email, crust, toppings) VALUES ('$email', '$crust', '$topString')";
+
+// 	// save to database and check for errors
+// 	if (mysqli_query($conn, $sql)) {
+// 		// redirect to home page
+// 		header('Location: index.php');
+// 	} else {
+// 		echo 'query error: ' . mysqli_error($conn);
+// 	}
+// }
+
+$title = $year = $rated = $length = $score = $description = '';
+$error = false;
+
+if (isset( $_POST['submit'] )) {
+	// print_r($_POST);
+
+	if ($_POST['title']) {
+		$title = $conn -> real_escape_string($_POST['title']);
+	} else {
+		echo 'No post title';
+		$error = true;
+	}
+
+	if ($_POST['year']) {
+		$year = $conn -> real_escape_string($_POST['year']);
+	} else {
+		echo 'No release year';
+		$error = true;
+	}
+
+	if ($_POST['rated']) {
+		$rated = $conn -> real_escape_string($_POST['rated']);
+	} else {
+		echo 'No parental rating';
+		$error = true;
+	}
+
+	if ($_POST['length']) {
+		$length = $conn -> real_escape_string($_POST['length']);
+	} else {
+		echo 'No length in minutes';
+		$error = true;
+	}
+
+	if ($_POST['score']) {
+		$score = $conn -> real_escape_string($_POST['score']);
+	} else {
+		echo 'No review score';
+		$error = true;
+	}
+
+	if ($_POST['description']) {
+		$description = $conn -> real_escape_string($_POST['description']);
+	} else {
+		echo 'No description';
+		$error = true;
+	}
+
+	if (!$error) {
+		// create SQL query
+		$sql = "INSERT INTO movies (title, score, year, rated, length, description) VALUES ('$title', '$score', '$year', '$rated', '$length', '$description')";
+
+		// save to database and check for errors
+		if ($conn -> query($sql)) {
+			// redirect to home page
+			header('Location: index.php');
 		} else {
-			echo 'Email address is not valid.';
-			$error = true;
-		}
-		$crust = $_POST['crust'];
-
-		for ($i = 1; $i < 5; $i++) {
-			array_push($toppings, $_POST["topping$i"]);
-		}
-
-		$toppings = array_filter($toppings);
-
-		if (!$error) {
-			$topString = implode(', ', $toppings);
-
-			// create SQL query
-			$sql = "INSERT INTO orders (email, crust, toppings) VALUES ('$email', '$crust', '$topString')";
-
-			// save to database and check for errors
-			if (mysqli_query($conn, $sql)) {
-				// redirect to home page
-				header('Location: index.php');
-			} else {
-				echo 'query error: ' . mysqli_error($conn);
-			}
+			echo 'query error: ' . mysqli_error($conn);
 		}
 	}
+
+}
 
 ?>
 
@@ -50,37 +109,31 @@
 
 	<main>
 		<div class="main-container">
-			<h2>Order A Pizza</h2>
-			<form action="order.php" method="post">
-				<label for="email">Email Address</label>
-				<input type="text" name="email" id="email" value=<?= $email ?> >
-				<label for="crust">Crust</label>
-				<select name="crust" id="crust">
-					<option value="hand-tossed" <?php if ($crust == 'hand-tossed') echo 'selected' ?> >Hand Tossed</option>
-					<option value="classic-pan" <?php if ($crust == 'classic-pan') echo 'selected' ?> >Classic Pan</option>
-					<option value="deep-dish" <?php if ($crust == 'deep-dish') echo 'selected' ?> >Deep Dish</option>
-				</select>
-				<h3>Toppings</h3>
-				<ul class="toppings">
-					<li>
-						<input type="checkbox" name="topping1" id="topping1" value="pepperoni" <?php if ($toppings[0]) echo 'checked' ?> >
-						<label for="topping1">Pepperoni</label>
-					</li>
-					<li>
-						<input type="checkbox" name="topping2" id="topping2" value="sausage" <?php if ($toppings[1]) echo 'checked' ?> >
-						<label for="topping2">Italian Sausage</label>
-					</li>
-					<li>
-						<input type="checkbox" name="topping3" id="topping3" value="pineapple" <?php if ($toppings[2]) echo 'checked' ?> >
-						<label for="topping3">Pineapple</label>
-					</li>
-					<li>
-						<input type="checkbox" name="topping4" id="topping4" value="bacon" <?php if ($toppings[3]) echo 'checked' ?> >
-						<label for="topping4">Bacon</label>
-					</li>
-				</ul>
-				<input type='submit' name='submit' value="Submit Order">
-			</form>
+			<div class="add-container">
+				<h2>Add A Movie</h2>
+				<hr>
+				<form action="addMovie.php" method="post">
+					<label for="title">Movie Title</label>
+					<input type="text" name="title" id="title" value="<?= $title ?>" >
+					<br />
+					<label for="year">Year Released</label>
+					<input type="text" name="year" id="year" value="<?= $year ?>" >
+					<br />
+					<label for="rated">Parental Rating</label>
+					<input type="text" name="rated" id="rated" value="<?= $rated ?>" >
+					<br />
+					<label for="length">Length in Minutes</label>
+					<input type="text" name="length" id="length" value="<?= $length ?>" >
+					<br />
+					<label for="score">Score</label>
+					<input type="text" name="score" id="score" value="<?= $score ?>" >
+					<br />
+					<label for="description">Description</label>
+					<input type="text" name="description" id="description" value="<?= $description ?>" >
+					<br />
+					<input type='submit' name='submit' value="Add Movie">
+				</form>
+			</div>
 		</div>
 	</main>
 
