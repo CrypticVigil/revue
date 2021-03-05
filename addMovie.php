0c672ba124818e64e8f992d8030e68b2b72659d1
Chad Barrows <chad.m.barrows@gmail.com>
2021-03-04 19:10:54 -0500

    improved styles on addMovie page

--- addMovie.php
+++ addMovie.php
@@ -6,7 +6,8 @@ $title = $year = $rated = $length = $score = $description = '';
 $error = false;
 
 if (isset( $_POST['submit'] )) {
-	// print_r($_POST);
+
+	// check if each piece of data has been entered and escape special characters
 
 	if ($_POST['title']) {
 		$title = $conn -> real_escape_string($_POST['title']);
@@ -79,23 +80,23 @@ if (isset( $_POST['submit'] )) {
 				<h2>Add A Movie</h2>
 				<hr>
 				<form action="addMovie.php" method="post">
-					<label for="title">Movie Title</label>
-					<input type="text" name="title" id="title" value="<?= $title ?>" >
+					<label for="title">Movie Title</label><br/>
+					<input type="text" name="title" id="title" value="<?= $title ?>" class="add-movie--input" >
 					<br />
-					<label for="year">Year Released</label>
-					<input type="text" name="year" id="year" value="<?= $year ?>" >
+					<label for="year">Year Released</label><br/>
+					<input type="text" name="year" id="year" value="<?= $year ?>" class="add-movie--input" >
 					<br />
-					<label for="rated">Parental Rating</label>
-					<input type="text" name="rated" id="rated" value="<?= $rated ?>" >
+					<label for="rated">Parental Rating</label><br/>
+					<input type="text" name="rated" id="rated" value="<?= $rated ?>" class="add-movie--input" >
 					<br />
-					<label for="length">Length in Minutes</label>
-					<input type="text" name="length" id="length" value="<?= $length ?>" >
+					<label for="length">Length in Minutes</label><br/>
+					<input type="text" name="length" id="length" value="<?= $length ?>" class="add-movie--input" >
 					<br />
-					<label for="score">Score</label>
-					<input type="text" name="score" id="score" value="<?= $score ?>" >
+					<label for="score">Score</label><br/>
+					<input type="text" name="score" id="score" value="<?= $score ?>" class="add-movie--input" >
 					<br />
-					<label for="description">Description</label>
-					<input type="text" name="description" id="description" value="<?= $description ?>" >
+					<label for="description">Description</label><br/>
+					<textarea name="description" id="description" rows=5 ><?= $description ?></textarea>
 					<br />
 					<input type='submit' name='submit' class="submit-button" value="Add Movie">
 				</form>
